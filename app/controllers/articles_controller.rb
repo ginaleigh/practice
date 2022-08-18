93102e336c9113fe7720db3f1482b0d4c30288b3
Gina Mirando <gina.mirando5@gmail.com>
2022-08-17 20:49:03 -0500

    modified

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @articles.as_json }
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
